55d82dc485b389ce346c6d1510202235d5bcae0b
Norah Almaneea <nouraalmanea0@gmail.com>
2024-09-16 23:45:58 +0300

    browser detector

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import BrowserDetector from "./browserDetector/browserDetector";
+
 const App = () => {
   return (
     <div>
       <h1>Test</h1>
+      <BrowserDetector />
     </div>
   );
 };
---
+++ src/browserDetector/browserDetector.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Bowser from "bowser";
+
+const BrowserDetector: React.FC = () => {
+  const userAgent = window.navigator.userAgent;
+  const browser = Bowser.getParser(userAgent);
+  const os = browser.getOSName();
+  const browserName = browser.getBrowserName();
+
+  return (
+    <div>
+      {browserName === "Safari" ? (
+        <button onClick={() => console.log("Apple Wallet Clicked")}>
+          Add to Apple Wallet
+        </button>
+      ) : (os === "Windows" || os === "Android") && browserName !== "Safari" ? (
+        <button onClick={() => console.log("Google Wallet Clicked")}>
+          Add to Google Wallet
+        </button>
+      ) : (
+        <p>Wallet option not available for this OS</p>
+      )}
+    </div>
+  );
+};
+
+export default BrowserDetector;
