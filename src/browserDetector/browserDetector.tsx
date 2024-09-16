cd13a0f40bf2bb1742aa8fdaa558a08916192937
Norah Almaneea <nouraalmanea0@gmail.com>
2024-09-17 00:35:03 +0300

    fix browser condition

--- src/browserDetector/browserDetector.tsx
+++ src/browserDetector/browserDetector.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Bowser from "bowser";
 
-const BrowserDetector: React.FC = () => {
+const BrowserDetector = () => {
   const userAgent = window.navigator.userAgent;
   const browser = Bowser.getParser(userAgent);
   const os = browser.getOSName();
@@ -13,7 +13,7 @@ const BrowserDetector: React.FC = () => {
         <button onClick={() => console.log("Apple Wallet Clicked")}>
           Add to Apple Wallet
         </button>
-      ) : (os === "Windows" || os === "Android") && browserName !== "Safari" ? (
+      ) : os === "Android" && browserName !== "Safari" ? (
         <button onClick={() => console.log("Google Wallet Clicked")}>
           Add to Google Wallet
         </button>
